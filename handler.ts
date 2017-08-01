7dc1f56258181e9b689e5d562626860ab6a7e8db
Fernando Paredes <nano@fdp.io>
2017-08-01 08:52:05 -0700

    Allow JSON or formdata for input

--- handler.ts
+++ handler.ts
@@ -1,5 +1,6 @@
 import * as AWS from 'aws-sdk'
 import * as nodemailer from 'nodemailer'
+import * as qs from 'qs'
 declare const process: any
 
 const region = process.env.SES_REGION
@@ -22,8 +23,12 @@ const createResponse = (
   let response: Response = {
     statusCode,
     body: JSON.stringify(body),
-    ...headers ? { headers } : {}
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      ...headers
+    }
   }
+
 
   return response
 }
@@ -63,13 +68,25 @@ async function sendEmail(
   })
 }
 
+const parseBody = (body) => {
+  let parsedBody
+
+  try {
+    parsedBody = JSON.parse(body)
+  } catch(error) {
+    parsedBody = qs.parse(body)
+  }
+
+  return parsedBody
+}
+
 export async function contact(event, context, callback) {
   // Avoids destructuring to ensure we have the right types
   const site: string = process.env.SITE
   const fromEmail: string = process.env.FROM_EMAIL
   const toEmail: string = process.env.TO_EMAIL
 
-  const body = JSON.parse(event.body)
+  const body = parseBody(event.body)
   const requiredFields = ['name', 'message', 'email']
   requiredFields.forEach(e => {
     if (!body.hasOwnProperty(e)) {
